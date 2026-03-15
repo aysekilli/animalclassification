c7a2525706633355b395f0f39e50fff213876e15
Ayşe <kll.aye.07@gmail.com>
2026-03-15 16:25:39 +0300

    more implementable

--- models/train.py
+++ models/train.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     model = YOLO("yolo11n-seg.pt")
 
     model.train(
-        data="C:/Users/ashe/Desktop/firstdataset/data.yaml",
+        data="../configs/data.yaml",
         epochs=50,
         imgsz=640,
         batch=16,
